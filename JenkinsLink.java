746266783e00548c8dc7908b105e43928ee1e2f4
PratheeshAnbu99 <117888815+PratheeshAnbu99@users.noreply.github.com>
2022-11-24 23:09:09 +0530

    Second Commit

--- JenkinsLink.java
+++ JenkinsLink.java
@@ -2,6 +2,8 @@ public class JenkinsLink{
 public static void main(String [] args){
 
 System.out.println("This is to create a link between jenkins and github");
+  System.out.println("This is to check whether the trigger action is happeneing");
+
 
 
 
