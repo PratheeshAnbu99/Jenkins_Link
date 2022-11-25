91c8bf1569cd999f2e71de2a340f20e4b5db56de
PratheeshAnbu99 <117888815+PratheeshAnbu99@users.noreply.github.com>
2022-11-25 12:31:51 +0530

    Fifteen commit

--- JenkinsLink.java
+++ JenkinsLink.java
@@ -5,15 +5,7 @@ System.out.println("This is to create a link between jenkins and github");
   System.out.println("This is to check whether the trigger action is happeneing");
     System.out.println("Finally It should run now");
    System.out.println("One final time");
-   System.out.println("Whats this da");
-  System.out.println("morning try");
-    System.out.println(" all set now");
-   System.out.println(" lets see now");
-   System.out.println(" lets see this now");
-    System.out.println("Github-jenkins integration");
-     System.out.println("Github-jenkins integration");
-  
-  
+
   
    
   
